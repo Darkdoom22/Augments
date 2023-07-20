f1e4647832ef909747505413ec45ec7c2320e224
Audrey Riley <darkdoom1337@gmail.com>
2023-07-19 23:20:26 -0700

    adjust accepted arguments to make it simpler on the lua side

--- Augments.cpp
+++ Augments.cpp
@@ -163,21 +163,29 @@ static int GetAugmentSystemFourData(lua_State* L)
 		return LuaCoreWrapper::oLuaL_Error(L, "Failed to find oGetAugmentSystemFourData function, the signature may have changed");
 	}
 
-	if(LuaCoreWrapper::oLua_GetTop(L) != 4)
+	if(LuaCoreWrapper::oLua_GetTop(L) != 2)
 	{
-		return LuaCoreWrapper::oLuaL_Error(L, "Invalid number of arguments passed to GetAugmentSystemFourData, expected 4 (exdatablock1, exdatablock2, exdatablock3, itemId)");
+		return LuaCoreWrapper::oLuaL_Error(L, "Invalid number of arguments passed to GetAugmentSystemFourData, expected 2 (item.extdata, item.id)");
 	}
 
-	//TODO: accept these in a more flexible/nicer way
-	const auto extDataBlock1 = static_cast<uint32_t>(LuaCoreWrapper::oLuaL_CheckInteger(L, 1));
-	const auto extDataBlock2 = static_cast<uint32_t>(LuaCoreWrapper::oLuaL_CheckInteger(L, 2));
-	const auto extDataBlock3 = static_cast<uint32_t>(LuaCoreWrapper::oLuaL_CheckInteger(L, 3));
-	const auto itemId = static_cast<uint16_t>(LuaCoreWrapper::oLuaL_CheckInteger(L, 4));
+	size_t strSize = 0;
+	const auto extDataString = LuaCoreWrapper::oLuaL_CheckLString(L, 1, &strSize);
+
+	if(strSize < 12)
+	{
+		return LuaCoreWrapper::oLuaL_Error(L, "Invalid extdata string passed to GetAugmentSystemFourData, expected at least 12 characters");
+	}
+
+	//can't rely on lua's string allocation to keep extDataString alive, so we have to copy it and manage it ourselves
+	const auto extData = std::make_unique<uint32_t[]>(3);
+	memcpy(extData.get(), extDataString, 12);
+
+	const auto itemId = static_cast<uint16_t>(LuaCoreWrapper::oLuaL_CheckInteger(L, 2));
 
 	auto augmentLookupItemData = AugmentLookupItemData_t{
-		extDataBlock1, //exdatablock1
-		extDataBlock2, //exdatablock2
-		extDataBlock3, //exdatablock3
+		extData[0], //exdatablock1
+		extData[1], //exdatablock2
+		extData[2], //exdatablock3
 		itemId, //itemId
 		0, //unk, only populated in memory for weapons that I saw but appears to work fine without it //TODO: figure out what this is
 		0, //unk, only populated in memory for weapons that I saw but appears to work fine without it
